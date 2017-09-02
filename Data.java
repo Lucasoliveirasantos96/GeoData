0278a3360e2d8c20cac822167632cca0985a8519
arqdsis <arqdsis@mocli10208.animaedu.intranet>
2017-09-01 22:39:17 -0300

    Initial commit

---
+++ Data.java
@@ -0,0 +1,35 @@
+package br.usjt.desmob.geodata;
+
+import java.util.ArrayList;
+
+/**
+ * Created by arqdsis on 01/09/2017.
+ */
+
+public class Data {
+
+
+    public static ArrayList <String> getPaises(){
+      ArrayList <String> paises = new ArrayList<>();
+        paises.add("Brasil");
+        paises.add("Argentina");
+        paises.add("Estados Unidos");
+        paises.add("Dinamarca");
+        paises.add("Angola");
+        paises.add("Egito");
+        paises.add("Inglaterra - Newcastle");
+        paises.add("Japão");
+        paises.add("Espanha");
+        paises.add("Costa Rica");
+        paises.add("Holanda");
+        paises.add("Luxemburgo");
+        paises.add("Peru");
+        paises.add("Venezuela");
+        paises.add("China");
+        paises.add("Suiça");
+        paises.add("Italia");
+        paises.add("Alemanha");
+        return paises;
+
+    }
+}
---
+++ Pais.java
@@ -0,0 +1,19 @@
+package br.usjt.desmob.geodata;
+
+/**
+ * Created by arqdsis on 01/09/2017.
+ */
+
+public class Pais {
+
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
